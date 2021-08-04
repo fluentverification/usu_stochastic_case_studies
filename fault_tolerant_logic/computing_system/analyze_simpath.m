1e0286e17533d43f9ab753a1bd0eca83e3860e62
Chris Winstead <winstead@trgenesys.bluezone.usu.edu>
2021-08-04 13:39:50 -0400

    Updated analysis script with variance calculations.

--- fault_tolerant_logic/computing_system/analyze_simpath.m
+++ fault_tolerant_logic/computing_system/analyze_simpath.m
@@ -14,7 +14,9 @@ end
 
 pfail = Nfail/Npath;
 
-fprintf(1,"Failure Probability \t lambda = %f\n", pfail);
+
+fprintf(1,"Failure Probability \t gamma = %f\n", pfail);
+fprintf(1,"Variance = gamma (Poisson statistics)\n\n");
 
 t = d.data(:,2);
 idx=find(s==0);
@@ -22,7 +24,9 @@ idx = idx(1:end-1);
 jdx = idx+1;
 kdx = idx(2:end);
 
-t0 = mean(t(kdx)-t(jdx(1:end-1)));
+t0    = mean(t(kdx)-t(jdx(1:end-1)));
+t0var = var(t(kdx)-t(jdx(1:end-1)));
 
 fprintf(1,"Return time \t\t tau_0  = %f\n", t0)
+fprintf(1,"Variance \t\t v_0  = %f\n", t0var)
 fprintf(1,"\t\t\t MTTF   = %f\n", t0/pfail)
