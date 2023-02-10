6d099c6175eddedfb231cd0a25289d7eae9799de
Eric Reiss <reiss_eric@hotmail.com>
2023-02-10 13:38:25 -0700

    Center probability integral on n

--- ngdbf_models/generate_gauss2.m
+++ ngdbf_models/generate_gauss2.m
@@ -9,13 +9,15 @@ pd = (1/(sig*sqrt(2*pi)))*exp(-0.5*((x-mu)/sig)^2);
 sig_p = 6;
 sig_n = -sig_p;
 %collect probability beyond sigma range
-p1 = int(pd, -inf, sig_n);
-p2 = int(pd, sig_p, inf);
+p1 = int(pd, -inf, sig_n+((2*sig_p)/200));
+p2 = int(pd, sig_p-((2*sig_p)/200), inf);
 %Compute discrete probabilities
 n = sig_n:(2*sig_p)/100:sig_p;
 pvec = zeros(1,101);
-for idx = 2:101
-    pvec(idx-1) = int(pd,n(idx-1),n(idx));
+for idx = 2:100
+    bound1 = n(idx)-(12/200);
+    bound2 = n(idx)+(12/200);
+    pvec(idx) = int(pd,bound1,bound2);
 end
-pvec(1) = p1+pvec(1);
+pvec(1) = p1;
 pvec(101) = p2;
