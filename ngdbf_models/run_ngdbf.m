09f1f0a60a2110a684d01e6898c2149695b34cab
Eric Reiss <reiss_eric@hotmail.com>
2023-03-24 14:53:44 -0600

    Generate samples and add iteration

--- ngdbf_models/run_ngdbf.m
+++ ngdbf_models/run_ngdbf.m
@@ -21,40 +21,52 @@ if check ~= 'y'
     th = input("Enter a new threshold: ");
     w = input("Enter a new weight: ");
 end
-constants = "-const ";
-if ~isempty(code_rate)
-    constants = constants+"r="+code_rate+",";
+base_constants = "-const ";
+if isempty(code_rate)
+    code_rate=0.8413;
 end
-if ~isempty(SNR)
-    constants = constants+"SNR="+SNR+",";
+if isempty(SNR)
+    SNR = 4.5;
+end   
+if isempty(th)
+    th = -0.55;
 end
-
-if ~isempty(th)
-    constants=constants+"theta="+th+",";
+if isempty(w)
+    w = 1/6;
 end
-
-if ~isempty(w)
-    constants=constants+"w="+w+",";
+base_constants = base_constants+"w="+w+","+"theta="+th+","+"SNR="+SNR+","+"r="+code_rate+",";
+iter = input("Enter number of iterations: ");
+iter = round(iter);
+result = zeros(1,iter);
+y1 = zeros(1,iter);
+y2 = zeros(1,iter);
+y3 = zeros(1,iter);
+for n = 1:iter
+    %% Generate samples
+    %TODO:actually generate samples
+    % The y values are gaussian with mean +1 and variance N0/2
+    sigma = sqrt(1/code_rate)*10^(-SNR/20);
+    y1(n) = normrnd(1,sigma);
+    y2(n) = normrnd(1,sigma);
+    y3(n) = normrnd(1,sigma);
+    
+    constants = base_constants+"y1="+y1(n)+",y2="+y2(n)+",y3="+y3(n);
+    
+    %% Select NGDBF Model
+    %Only the dtmc binary model is complete
+    model_path = "binary/dtmc_ngdbf_3bit.prism";
+    prop_path = "binary/halt_dtmc.pctl";
+    
+    %% Simulate Model and Capture Output
+    [status,output] = system("prism "+ model_path +" "+prop_path+" "+constants);
+    if status == 1
+        fprintf("%s",output);
+    else
+    %% Process output
+    %extract result probability
+    temp1 = regexp(output,"Result: ",'split'); 
+    temp2 = split(temp1(2),' ');
+    result(n) = str2double(temp2(1));
+    fprintf("Result: %f with samples %f,%f,%f\n",result(n),y1(n),y2(n),y3(n));
+    end
 end
-
-%% Generate samples
-%TODO:actually generate samples
-
-%Test values
-y1 = 0.4;
-y2 = -1.2;
-y3 = 0.6353;
-
-constants = constants+"y1="+y1+",y2="+y2+",y3="+y3;
-%% Select NGDBF Model
-%Only the dtmc binary model is complete
-model_path = "binary/dtmc_ngdbf_3bit.prism";
-prop_path = "binary/halt_dtmc.pctl";
-%% Simulate Model and Capture Output
-[status,output] = system("prism "+ model_path +" "+prop_path+" "+constants);
-
-%% Process output
-%extract result probability
-temp1 = regexp(output,"Result: ",'split'); 
-temp2 = split(temp1(2),' ');
-result = str2double(temp2(1));
