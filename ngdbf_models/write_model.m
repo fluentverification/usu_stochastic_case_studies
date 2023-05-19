718711eeb530cefa5c28c6aad4d4ce217693dbef
Eric Reiss <reiss_eric@hotmail.com>
2023-05-19 11:49:57 -0700

    Add file writer script

---
+++ ngdbf_models/write_model.m
@@ -0,0 +1,64 @@
+function status = write_model(fileName,y1,y2,y3,p)
+    arguments
+        fileName string
+        y1 double
+        y2 double
+        y3 double
+        p (8,8) double
+    end
+    if y1 > 0
+        x1 = 0;
+    else
+        x1 = 1;
+    end
+    if y2 > 0
+        x2 = 0;
+    else
+        x2 = 1;
+    end
+    if y3 > 0
+        x3 = 0;
+    else
+        x3 = 1;
+    end
+    state = round((x3*1+x2*2+x1*4)+1);
+    fid = fopen(fileName,'w');
+    fprintf(fid,"dtmc\n\n");
+    fprintf(fid,"formula s1 = mod(x2+x3,2);\n");
+    fprintf(fid,"formula s2 = mod(x1+x3,2);\n");
+    fprintf(fid,"formula s3 = mod(x1+x3,2);\n");
+    fprintf(fid,"formula done = (s1 = 0) & (s2 = 0) & (s3 = 0);\n\n");
+    fprintf(fid,"module trapping_set \n");
+    fprintf(fid,"\tstate : [1..8] init %d;\n",state);
+    fprintf(fid,"\tx1 : [0..1] init %d;\n",x1);
+    fprintf(fid,"\tx2 : [0..1] init %d;\n",x2);
+    fprintf(fid,"\tx3 : [0..1] init %d;\n",x3);
+    fprintf(fid,"\thalt : bool init false;\n\n");
+    fprintf(fid,"\t[] done -> 1:(halt' = true);\n");
+    for i = 1:8
+        fprintf(fid,"\t[] state=%d & !done -> ",i);
+        for j=1:8
+            fprintf(fid,"%.8f:(state'=%d)",p(i,j),j);
+            cur_state = dec2bin(i,3);
+            update_state = dec2bin(j,3);
+            if cur_state(1) ~= update_state(1)
+                fprintf(fid,"&(x1' = mod(x1+1,2))");
+            end
+            if cur_state(2) ~= update_state(2)
+                fprintf(fid,"&(x2' = mod(x2+1,2))");
+            end
+            if cur_state(3) ~= update_state(3)
+                fprintf(fid,"&(x3' = mod(x3+1,2))");
+            end
+            if j ~= 8
+                fprintf(fid,"+");
+            end
+        end
+        fprintf(fid,";\n");
+    end
+
+    fprintf(fid,"endmodule\n");
+    status = 0;
+    fclose(fid);
+
+end
