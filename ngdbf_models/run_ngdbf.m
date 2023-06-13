b8f943595806012f1bc6fa75656ef1f783580b1c
Eric Reiss <reiss_eric@hotmail.com>
2023-06-13 13:19:41 -0600

    Generalize run_ngdbf for other trapping sets

--- ngdbf_models/run_ngdbf.m
+++ ngdbf_models/run_ngdbf.m
@@ -45,38 +45,35 @@ file_out = fopen("output.txt","w");
 % Set number of times to run
 %rounds = input("Enter number of times to simulate: ");
 %rounds = round(rounds);
-E = 8; % Test every error combination
-result = zeros(1,E);
+error_total = 8; % Test every error combination
+var_size = 3; % Number of variable nodes
+result = zeros(1,error_total);
 %initialize input history
-y1 = zeros(1,E);
-y2 = zeros(1,E);
-y3 = zeros(1,E);
+y = zeros(var_size,error_total);
+% y2 = zeros(1,error_total);
+% y3 = zeros(1,error_total);
 
 % Run simulations
-for n = 1:E
-    % The y values are gaussian with mean +1 and variance N0/2
-    % magnitude is taken so that error position can be determined
+for idx = 1:error_total
     sigma = sqrt(1/code_rate)*10^(-SNR/20);
-    y1(n) = abs(normrnd(1,sigma));
-    y2(n) = abs(normrnd(1,sigma));
-    y3(n) = abs(normrnd(1,sigma));
+    bin_pos = dec2bin(idx-1,3);
+    x_state = zeros(1,var_size);
+%     y1(position) = abs(normrnd(1,sigma));
+%     y2(position) = abs(normrnd(1,sigma));
+%     y3(position) = abs(normrnd(1,sigma));
+    for n = 1:var_size
+        % The y values are gaussian with mean +1 and variance N0/2
+        % magnitude is taken so that error position can be determined
+        y(n,idx) = abs(normrnd(1,sigma));
+        %Determine error position
+        if bin_pos(n) == '1'
+            y(n,idx) = -y(n,idx);
+        end
+        % Set initial node values
+        x_state(n) = sign(y(n,idx));
+    end
 
-    %Determine error position
-    bin_n = dec2bin(n-1,3);
-    if bin_n(1) == '1'
-        y1(n) = -y1(n);
-    end
-    if bin_n(2) == '1' 
-        y2(n) = -y2(n);
-    end
-    if bin_n(3) == '1' 
-        y3(n) = -y3(n);
-    end
-
-    % Set initial node values
-    x1 = sign(y1(n));
-    x2 = sign(y2(n));
-    x3 = sign(y3(n));
+    
     
     % Create state matrix
     % States taken from Tasnuva dissertation (Table 3.1, pg 27)
@@ -88,7 +85,7 @@ for n = 1:E
     % state 6: -1  1 -1 | 1 0 1
     % state 7: -1 -1  1 | 1 1 0
     % state 8: -1 -1 -1 | 1 1 1
-    x = [
+    x_state = [
         1 1 1;
         1 1 -1;
         1 -1 1;
@@ -99,139 +96,52 @@ for n = 1:E
         -1 -1 -1];
 
     %initialize Energy matrix
-    E = zeros(8,3);
+    error_total = zeros(8,3);
     % Calculate all possible energy values for each state
-    for j = 1:8
-        s1 = x(j,2)*x(j,3);
-        s2 = x(j,1)*x(j,3);
-        s3 = x(j,1)*x(j,3);
-        s4 = x(j,1);
-        s5 = x(j,2);
-        s6 = x(j,3);
-        E(j,1) = y1(n)*x(j,1) + w*(s3+s2+s4);
-        E(j,2) = y2(n)*x(j,2) + w*(s3+s1+s5);
-        E(j,3) = y3(n)*x(j,3)+w*(s1+s2+s6);
+    for row = 1:8
+        s1 = x_state(row,2)*x_state(row,3);
+        s2 = x_state(row,1)*x_state(row,3);
+        s3 = x_state(row,1)*x_state(row,3);
+        s4 = x_state(row,1);
+        s5 = x_state(row,2);
+        s6 = x_state(row,3);
+        error_total(row,1) = y1(idx)*x_state(row,1) + w*(s3+s2+s4);
+        error_total(row,2) = y2(idx)*x_state(row,2) + w*(s3+s1+s5);
+        error_total(row,3) = y3(idx)*x_state(row,3)+w*(s1+s2+s6);
     end
 
     %% calculate transition probabilities
     p = zeros(8,8);
     % Flip probabilities calculated according to Eq 3.13 in Tasnuva
     % dissertation (pg. 26)
-    row = 1;
-    % State 1 (1 1 1)
-    px1 = normcdf(theta,E(row,1),sigma);
-    px2 = normcdf(theta,E(row,2),sigma);
-    px3 = normcdf(theta,E(row,3),sigma);
-    p(row,1) = (1-px1)*(1-px2)*(1-px3);
-    p(row,2) = (1-px1)*(1-px2)*px3;
-    p(row,3) = (1-px1)*px2*(1-px3);
-    p(row,4) = (1-px1)*px2*px3;
-    p(row,5) = px1*(1-px2)*(1-px3);
-    p(row,6) = px1*(1-px2)*px3;
-    p(row,7) = px1*px2*(1-px3);
-    p(row,8) = px1*px2*px3;
-
-    % State 2 (1 1 -1)
-    row = 2;
-    px1 = normcdf(theta,E(row,1),sigma);
-    px2 = normcdf(theta,E(row,2),sigma);
-    px3 = normcdf(theta,E(row,3),sigma);
-    p(row,2) = (1-px1)*(1-px2)*(1-px3);
-    p(row,1) = (1-px1)*(1-px2)*px3;
-    p(row,4) = (1-px1)*px2*(1-px3);
-    p(row,3) = (1-px1)*px2*px3;
-    p(row,6) = px1*(1-px2)*(1-px3);
-    p(row,5) = px1*(1-px2)*px3;
-    p(row,8) = px1*px2*(1-px3);
-    p(row,7) = px1*px2*px3;
-
-    % State 3 ( 1 -1 1)
-    row = 3;
-    px1 = normcdf(theta,E(row,1),sigma);
-    px2 = normcdf(theta,E(row,2),sigma);
-    px3 = normcdf(theta,E(row,3),sigma);
-    p(row,3) = (1-px1)*(1-px2)*(1-px3); % ( 1 -1 1) 
-    p(row,4) = (1-px1)*(1-px2)*px3; % (1 -1 -1) 
-    p(row,1) = (1-px1)*px2*(1-px3); % (1 1 1) 
-    p(row,2) = (1-px1)*px2*px3; %(1 1 -1) 
-    p(row,7) = px1*(1-px2)*(1-px3); %(-1 -1 1) 
-    p(row,8) = px1*(1-px2)*px3; %(-1 -1 -1) 
-    p(row,5) = px1*px2*(1-px3); %(-1 1 1)
-    p(row,6) = px1*px2*px3;
-
-    % State 4 (1 -1 -1)
-    row = 4;
-    px1 = normcdf(theta,E(row,1),sigma);
-    px2 = normcdf(theta,E(row,2),sigma);
-    px3 = normcdf(theta,E(row,3),sigma);
-    p(row,4) = (1-px1)*(1-px2)*(1-px3);
-    p(row,3) = (1-px1)*(1-px2)*px3; % (1 -1 1)
-    p(row,2) = (1-px1)*px2*(1-px3); % (1 1 -1)
-    p(row,1) = (1-px1)*px2*px3; % (1 1 1)
-    p(row,8) = px1*(1-px2)*(1-px3); % (-1 -1 -1)
-    p(row,7) = px1*(1-px2)*px3; % (-1 -1 1)
-    p(row,6) = px1*px2*(1-px3); % (-1 1 -1
-    p(row,5) = px1*px2*px3;
-
-    % State 5 (-1 1 1)
-    row = 5;
-    px1 = normcdf(theta,E(row,1),sigma);
-    px2 = normcdf(theta,E(row,2),sigma);
-    px3 = normcdf(theta,E(row,3),sigma);
-    p(row,5) = (1-px1)*(1-px2)*(1-px3);
-    p(row,6) = (1-px1)*(1-px2)*px3; % (-1 1 -1)
-    p(row,7) = (1-px1)*px2*(1-px3); % (-1 -1 1)
-    p(row,8) = (1-px1)*px2*px3; % (-1 -1 -1)
-    p(row,1) = px1*(1-px2)*(1-px3); % (1 1 1)
-    p(row,2) = px1*(1-px2)*px3; % (1 1 -1)
-    p(row,3) = px1*px2*(1-px3); % (1 -1 1)
-    p(row,4) = px1*px2*px3;
-
-    % State 6 (-1 1 -1)
-    row = 6;
-    px1 = normcdf(theta,E(row,1),sigma);
-    px2 = normcdf(theta,E(row,2),sigma);
-    px3 = normcdf(theta,E(row,3),sigma);
-    p(row,6) = (1-px1)*(1-px2)*(1-px3);
-    p(row,5) = (1-px1)*(1-px2)*px3; % (-1 1 1)
-    p(row,8) = (1-px1)*px2*(1-px3); % (-1 -1 -1)
-    p(row,7) = (1-px1)*px2*px3; % (-1 -1 1)
-    p(row,2) = px1*(1-px2)*(1-px3); % (1 1 -1)
-    p(row,1) = px1*(1-px2)*px3;
-    p(row,4) = px1*px2*(1-px3); %(1 -1 -1)
-    p(row,3) = px1*px2*px3;
-
-    % State 7 (-1 -1 1)
-    row = 7;
-    px1 = normcdf(theta,E(row,1),sigma);
-    px2 = normcdf(theta,E(row,2),sigma);
-    px3 = normcdf(theta,E(row,3),sigma);
-    p(row,7) = (1-px1)*(1-px2)*(1-px3);
-    p(row,8) = (1-px1)*(1-px2)*px3;
-    p(row,5) = (1-px1)*px2*(1-px3); % (-1 1 1)
-    p(row,6) = (1-px1)*px2*px3;
-    p(row,3) = px1*(1-px2)*(1-px3);
-    p(row,4) = px1*(1-px2)*px3;
-    p(row,1) = px1*px2*(1-px3);
-    p(row,2) = px1*px2*px3;
-
-    % State 8 (-1 -1 -1)
-    row = 8;
-    px1 = normcdf(theta,E(row,1),sigma);
-    px2 = normcdf(theta,E(row,2),sigma);
-    px3 = normcdf(theta,E(row,3),sigma);
-    p(row,8) = (1-px1)*(1-px2)*(1-px3);
-    p(row,7) = (1-px1)*(1-px2)*px3;
-    p(row,6) = (1-px1)*px2*(1-px3); % (-1 1 -1)
-    p(row,5) = (1-px1)*px2*px3;
-    p(row,4) = px1*(1-px2)*(1-px3);
-    p(row,3) = px1*(1-px2)*px3;
-    p(row,2) = px1*px2*(1-px3);
-    p(row,1) = px1*px2*px3;
+    for row = 1:8
+        px1 = normcdf(theta,error_total(row,1),sigma);
+        px2 = normcdf(theta,error_total(row,2),sigma);
+        px3 = normcdf(theta,error_total(row,3),sigma);
+        rowbin = dec2bin(row-1,3);
+        for col = 1:8
+            colbin = dec2bin(col-1,3);
+            if rowbin(1) == colbin(1)
+                p(row,col) = 1-px1;
+            else
+                p(row,col) = px1;
+            end
+            if rowbin(2) == colbin(2)
+                p(row,col) = p(row,col)*(1-px2);
+            else
+                p(row,col) = p(row,col)*px2;
+            end
+            if rowbin(3) == colbin(3)
+                p(row,col) = p(row,col)*(1-px3);
+            else
+                p(row,col) = p(row,col)*px3;
+            end
+        end
+    end
     
     if sum(round(sum(p.'))) ~= 8
         fprintf("Error: Probabilities do not sum to 1\n");
-        exit();
+        return;
     end
 
     %% Write File
@@ -240,8 +150,8 @@ for n = 1:E
     % Select NGDBF Model
     % Only the dtmc binary model is complete
      model_path = "binary/dtmc_ngdbf_3bit.prism";
-     prop_path = "binary/halt_dtmc.pctl";
-     [stat, istate] = write_model(model_path,y1(n),y2(n),y3(n),p);
+    % prop_path = "binary/halt_dtmc.pctl";
+     [stat, istate] = write_model(model_path,y1(idx),y2(idx),y3(idx),p);
     % Simulate Model and Capture Output
      [status,output] = system("prism "+ model_path +" "+tag);
       if status == 1
@@ -253,23 +163,23 @@ for n = 1:E
 %         temp1 = regexp(output,"Result: ",'split'); 
 %         temp2 = split(temp1(2),' ');
 %         result(n) = str2double(temp2(1));
-        if y1(n) > 0
+        if y1(idx) > 0
             x1 = 0;
         else
             x1 = 1;
         end
-        if y2(n) > 0
+        if y2(idx) > 0
             x2 = 0;
         else
             x2 = 1;
         end
-        if y3(n) > 0
+        if y3(idx) > 0
             x3 = 0;
         else
             x3 = 1;
         end
 
-        fprintf(file_out,"%s\n\n%d:\tResult: N/A with samples %e (%d),%e (%d),%e (%d) \t initial state: %d\n-------------------------\n",output,n,y1(n),x1,y2(n),x2,y3(n),x3,istate);
+        fprintf(file_out,"%s\n\n%d:\tResult: N/A with samples %e (%d),%e (%d),%e (%d) \t initial state: %d\n-------------------------\n",output,idx,y1(idx),x1,y2(idx),x2,y3(idx),x3,istate);
       end
 
 end
