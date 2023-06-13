65218953f521c992d6eb396e95ed03a75029bfd4
Eric Reiss <reiss_eric@hotmail.com>
2023-06-13 16:43:44 -0600

    Generalized script to handle other trapping sets

--- ngdbf_models/run_ngdbf.m
+++ ngdbf_models/run_ngdbf.m
@@ -1,186 +1,173 @@
 % Wrapper for NGDBF Prism Models
-clear;clc;
-% Ask for inputs
-fprintf("Default Values\n");
-fprintf("Code Rate = 0.8413\n");
-fprintf("SNR = 4.5\n");
-fprintf("Threshold = -0.55\n");
-fprintf("Weight = 1/6 (%f) \n",1/6);
-fprintf("Transient probability calculation with 600 iterations ( -tr 600 ) \n");
-check = input("Would you like to use the default values [y/n]? ","s");
-if isempty(check)
-    check = 'y';
-end
-code_rate = [];
-SNR = [];
-theta = [];
-w = [];
-tag = [];
-if check ~= 'y'
-    fprintf("Press enter to use default value\n");
-    code_rate = input("Enter a new code rate: ");
-    SNR = input("Enter a new SNR: ");
-    theta = input("Enter a new threshold: ");
-    w = input("Enter a new weight: ");
-    tag = input("Enter a new simulation tag or property (ie -tr 500, -ss, -prop <property>, etc...): ",'s');
-end
-base_constants = "-const ";
-if isempty(code_rate)
-    code_rate=0.8413;
-end
-if isempty(SNR)
-    SNR = 4.5;
-end   
-if isempty(theta)
-    theta = -0.55;
-end
-if isempty(w)
-    w = 1/6;
-end
-if isempty(tag)
-    tag = "-tr 600";
-end
-
-file_out = fopen("output.txt","w");
-% Set number of times to run
-%rounds = input("Enter number of times to simulate: ");
-%rounds = round(rounds);
-error_total = 8; % Test every error combination
-var_size = 3; % Number of variable nodes
-result = zeros(1,error_total);
-%initialize input history
-y = zeros(var_size,error_total);
-% y2 = zeros(1,error_total);
-% y3 = zeros(1,error_total);
-
-% Run simulations
-for idx = 1:error_total
-    sigma = sqrt(1/code_rate)*10^(-SNR/20);
-    bin_pos = dec2bin(idx-1,3);
-    x_state = zeros(1,var_size);
-%     y1(position) = abs(normrnd(1,sigma));
-%     y2(position) = abs(normrnd(1,sigma));
-%     y3(position) = abs(normrnd(1,sigma));
-    for n = 1:var_size
-        % The y values are gaussian with mean +1 and variance N0/2
-        % magnitude is taken so that error position can be determined
-        y(n,idx) = abs(normrnd(1,sigma));
-        %Determine error position
-        if bin_pos(n) == '1'
-            y(n,idx) = -y(n,idx);
+function [p,y] = run_ngdbf(trans_mat)
+    clc;
+    if width(trans_mat) ~= length(trans_mat)
+        fprintf("Error: Dimensions of transition matrix do not match\n");
+        return
+    end
+   
+    % Ask for inputs
+    fprintf("Default Values\n");
+    fprintf("Code Rate = 0.8413\n");
+    fprintf("SNR = 4.5\n");
+    fprintf("Threshold = -0.55\n");
+    fprintf("Weight = 1/6 (%f) \n",1/6);
+    fprintf("Transient probability calculation with 600 iterations ( -tr 600 ) \n");
+    check = input("Would you like to use the default values [y/n]? ","s");
+    if isempty(check)
+        check = 'y';
+    end
+    code_rate = [];
+    SNR = [];
+    theta = [];
+    w = [];
+    tag = [];
+    if check ~= 'y'
+        fprintf("Press enter to use default value\n");
+        code_rate = input("Enter a new code rate: ");
+        SNR = input("Enter a new SNR: ");
+        theta = input("Enter a new threshold: ");
+        w = input("Enter a new weight: ");
+        tag = input("Enter a new simulation tag or property (ie -tr 500, -ss, -prop <property>, etc...): ",'s');
+    end
+  
+    if isempty(code_rate)
+        code_rate=0.8413;
+    end
+    if isempty(SNR)
+        SNR = 4.5;
+    end   
+    if isempty(theta)
+        theta = -0.55;
+    end
+    if isempty(w)
+        w = 1/6;
+    end
+    if isempty(tag)
+        tag = "-tr 600";
+    end
+    
+    file_out = fopen("output.txt","w"); % Open output file
+    var_size = length(trans_mat); % Number of variable nodes
+    check_size = sum(sum(trans_mat))-var_size; % Number of check nodes
+    error_total = 2^var_size; % Number of possible errors
+    y = zeros(1,var_size); %initialize inputs
+    
+    % Run simulations
+    for idx = 1:error_total
+        sigma = sqrt(1/code_rate)*10^(-SNR/20);
+        bin_pos = dec2bin(idx-1,var_size);
+        x_sign = zeros(1,var_size);
+        %Produce y values
+        for n = 1:var_size
+            % The y values are gaussian with mean +1 and variance N0/2
+            % magnitude is taken so that error position can be determined
+            y(n) = abs(normrnd(1,sigma));
+            %Determine error position
+            if bin_pos(n) == '1'
+                y(n) = -y(n);
+            end
+            % Set initial node values
+            x_sign(n) = sign(y(n));
         end
-        % Set initial node values
-        x_state(n) = sign(y(n,idx));
-    end
-
     
-    
-    % Create state matrix
-    % States taken from Tasnuva dissertation (Table 3.1, pg 27)
-    % state 1:  1  1  1 | 0 0 0 
-    % state 2:  1  1 -1 | 0 0 1
-    % state 3:  1 -1  1 | 0 1 0
-    % state 4:  1 -1 -1 | 0 1 1
-    % state 5: -1  1  1 | 1 0 0 
-    % state 6: -1  1 -1 | 1 0 1
-    % state 7: -1 -1  1 | 1 1 0
-    % state 8: -1 -1 -1 | 1 1 1
-    x_state = [
-        1 1 1;
-        1 1 -1;
-        1 -1 1;
-        1 -1 -1;
-        -1 1 1;
-        -1 1 -1;
-        -1 -1 1;
-        -1 -1 -1];
-
-    %initialize Energy matrix
-    error_total = zeros(8,3);
-    % Calculate all possible energy values for each state
-    for row = 1:8
-        s1 = x_state(row,2)*x_state(row,3);
-        s2 = x_state(row,1)*x_state(row,3);
-        s3 = x_state(row,1)*x_state(row,3);
-        s4 = x_state(row,1);
-        s5 = x_state(row,2);
-        s6 = x_state(row,3);
-        error_total(row,1) = y1(idx)*x_state(row,1) + w*(s3+s2+s4);
-        error_total(row,2) = y2(idx)*x_state(row,2) + w*(s3+s1+s5);
-        error_total(row,3) = y3(idx)*x_state(row,3)+w*(s1+s2+s6);
-    end
-
-    %% calculate transition probabilities
-    p = zeros(8,8);
-    % Flip probabilities calculated according to Eq 3.13 in Tasnuva
-    % dissertation (pg. 26)
-    for row = 1:8
-        px1 = normcdf(theta,error_total(row,1),sigma);
-        px2 = normcdf(theta,error_total(row,2),sigma);
-        px3 = normcdf(theta,error_total(row,3),sigma);
-        rowbin = dec2bin(row-1,3);
-        for col = 1:8
-            colbin = dec2bin(col-1,3);
-            if rowbin(1) == colbin(1)
-                p(row,col) = 1-px1;
-            else
-                p(row,col) = px1;
-            end
-            if rowbin(2) == colbin(2)
-                p(row,col) = p(row,col)*(1-px2);
-            else
-                p(row,col) = p(row,col)*px2;
-            end
-            if rowbin(3) == colbin(3)
-                p(row,col) = p(row,col)*(1-px3);
-            else
-                p(row,col) = p(row,col)*px3;
+        % Create state matrix
+        % States taken from Tasnuva dissertation (Table 3.1, pg 27)
+        x_str = [dec2bin(0:(2^var_size)-1,var_size)];
+        x = zeros(2^var_size,var_size);
+        % convert to bipolar states
+        for row = 1:2^var_size
+            for col = 1:var_size
+                  x(row,col) = str2num(x_str(row,col));
+                if x(row,col) == 1
+                    x(row,col) = -1;
+                else
+                    x(row,col) = 1;
+                end
             end
         end
-    end
+        %x = int(x);
     
-    if sum(round(sum(p.'))) ~= 8
-        fprintf("Error: Probabilities do not sum to 1\n");
-        return;
-    end
-
-    %% Write File
-    % constants = base_constants+"y1="+y1(n)+",y2="+y2(n)+",y3="+y3(n);
+        %initialize Energy and check node matrices
+        E = zeros(2^var_size,var_size);
+        s = zeros(1, check_size);
+        s_sum = zeros(1,var_size);
+        % Calculate all possible energy values for each state
+        for row = 1:2^var_size
+            % Calculate all check nodes
+            s_idx = 1;
+            for tran_row = 1:length(trans_mat)
+                for tran_col = 1:tran_row
+                    if trans_mat(tran_row,tran_col) == 1
+                        if tran_row == tran_col
+                            s(s_idx) = x(row,tran_row);
+                            s_sum(tran_row) = s_sum(tran_row)+s(s_idx);
+                        else
+                            s(s_idx) = x(row,tran_row)*x(row,tran_col);
+                            s_sum(tran_row) = s_sum(tran_row)+s(s_idx);
+                            s_sum(tran_col) = s_sum(tran_col)+s(s_idx);
+                        end
+                        s_idx = s_idx+1;
+                    end
+                end
+            end
+            % Calculate energy values
+            for E_idx = 1:var_size
+                E(row,E_idx) = y(E_idx)*x(row,E_idx)+w*s_sum(E_idx);
+            end
+        end
     
-    % Select NGDBF Model
-    % Only the dtmc binary model is complete
-     model_path = "binary/dtmc_ngdbf_3bit.prism";
-    % prop_path = "binary/halt_dtmc.pctl";
-     [stat, istate] = write_model(model_path,y1(idx),y2(idx),y3(idx),p);
-    % Simulate Model and Capture Output
-     [status,output] = system("prism "+ model_path +" "+tag);
-      if status == 1
-         fprintf("%s\n",output);
-         return;
-     else
-        %% Process output
-        % extract result probability
-%         temp1 = regexp(output,"Result: ",'split'); 
-%         temp2 = split(temp1(2),' ');
-%         result(n) = str2double(temp2(1));
-        if y1(idx) > 0
-            x1 = 0;
-        else
-            x1 = 1;
+        %% calculate transition probabilities
+        p = ones(2^var_size,2^var_size);
+        % Flip probabilities calculated according to Eq 3.13 in Tasnuva
+        % dissertation (pg. 26)
+        for row = 1:2^var_size
+            px = zeros(1,var_size);
+            for p_idx = 1:var_size
+                px(p_idx) = normcdf(theta,E(row,p_idx),sigma);
+            end
+            rowbin = dec2bin(row-1,var_size);
+            for col = 1:2^var_size
+                colbin = dec2bin(col-1,var_size);
+                for p_idx = 1:var_size
+                    if rowbin(p_idx) == colbin(p_idx)
+                        p(row,col) = p(row,col)*(1-px(p_idx));
+                    else
+                        p(row,col) = p(row,col)*px(p_idx);
+                    end
+                end
+            end
         end
-        if y2(idx) > 0
-            x2 = 0;
-        else
-            x2 = 1;
+        
+        if sum(round(sum(p.'))) ~= 2^var_size
+            fprintf("Error: Probabilities do not sum to 1\n");
+            return;
         end
-        if y3(idx) > 0
-            x3 = 0;
-        else
-            x3 = 1;
-        end
-
-        fprintf(file_out,"%s\n\n%d:\tResult: N/A with samples %e (%d),%e (%d),%e (%d) \t initial state: %d\n-------------------------\n",output,idx,y1(idx),x1,y2(idx),x2,y3(idx),x3,istate);
-      end
-
+    
+        %% Write File
+        % constants = base_constants+"y1="+y1(n)+",y2="+y2(n)+",y3="+y3(n);
+        
+        % Select NGDBF Model
+        % Only the dtmc binary model is complete
+         model_path = "binary/dtmc_ngdbf_3bit.prism";
+        % prop_path = "binary/halt_dtmc.pctl";
+         [stat, istate] = write_model(model_path,y,p);
+        % Simulate Model and Capture Output
+         [status,output] = system("prism "+ model_path +" "+tag);
+          if status == 1 || stat == 1
+             fprintf("%s\n",output);
+             return;
+         else
+            %% Process output
+            % extract result probability
+    %         temp1 = regexp(output,"Result: ",'split'); 
+    %         temp2 = split(temp1(2),' ');
+    %         result(n) = str2double(temp2(1));
+            
+     
+            fprintf(file_out,"%s\ninitial state: %d\n----------------------------------------------------------------------------------------------------\n\n",output,istate);
+          end
+    
+    end
+    fclose(file_out);
 end
-fclose(file_out);
--- ngdbf_models/write_model.m
+++ ngdbf_models/write_model.m
@@ -1,65 +1,34 @@
-function [status,state] = write_model(fileName,y1,y2,y3,p)
-    arguments
-        fileName string
-        y1 double
-        y2 double
-        y3 double
-        p (8,8) double
+function [status,state] = write_model(fileName,y,p)
+    y_size = length(y);
+    x = zeros(1,y_size);
+    state = 1;
+    for idx = 1:y_size
+        if y(idx) > 0
+            x(idx) = 0;
+        else
+            x(idx) = 1;
+        end
+        state = state+(x(idx)*2^(idx-1));
     end
-    if y1 > 0
-        x1 = 0;
-    else
-        x1 = 1;
-    end
-    if y2 > 0
-        x2 = 0;
-    else
-        x2 = 1;
-    end
-    if y3 > 0
-        x3 = 0;
-    else
-        x3 = 1;
-    end
-    state = round((x3*1+x2*2+x1*4)+1);
+  
     fid = fopen(fileName,'w');
     fprintf(fid,"dtmc\n\n");
-    fprintf(fid,"formula s1 = mod(x2+x3,2);\n");
-    fprintf(fid,"formula s2 = mod(x1+x3,2);\n");
-    fprintf(fid,"formula s3 = mod(x1+x3,2);\n");
-    fprintf(fid,"formula s4 = x1;\n");
-    fprintf(fid,"formula s5 = x2;\n");
-    fprintf(fid,"formula s6 = x3;\n");
-    %fprintf(fid,"formula done = (s1 = 0) & (s2 = 0) & (s3 = 0)& (s4=0) & (s5 = 0) & (s6 = 0);//(state=1);\n\n");
     fprintf(fid,"module trapping_set \n");
-    fprintf(fid,"\tstate : [1..8] init %d;\n",state);
-    fprintf(fid,"\tx1 : [0..1] init %d;\n",x1);
-    fprintf(fid,"\tx2 : [0..1] init %d;\n",x2);
-    fprintf(fid,"\tx3 : [0..1] init %d;\n",x3);
-    fprintf(fid,"\thalt : bool init false;\n\n");
+    fprintf(fid,"\tstate : [1..%d] init %d;\n",2^y_size,state);
+    %fprintf(fid,"\thalt : bool init false;\n\n");
     %fprintf(fid,"\t[finish] done -> 1:(halt' = true);\n");
-    for i = 1:8
+    for i = 1:2^y_size
         if i ~= 1
             fprintf(fid,"\t[] state=%d -> ",i);
-            for j=1:8
+            for j=1:2^y_size
                 fprintf(fid,"%.8f:(state'=%d)",p(i,j),j);
-                cur_state = dec2bin(i-1,3);
-                update_state = dec2bin(j-1,3);
-                if cur_state(1) ~= update_state(1)
-                    fprintf(fid,"&(x1' = mod(x1+1,2))");
-                end
-                if cur_state(2) ~= update_state(2)
-                    fprintf(fid,"&(x2' = mod(x2+1,2))");
-                end
-                if cur_state(3) ~= update_state(3)
-                    fprintf(fid,"&(x3' = mod(x3+1,2))");
-                end
-                if j ~= 8
+                if j ~= 2^y_size
                     fprintf(fid,"+");
                 end
             end
         else
-            fprintf(fid,"\t[finish] state=%d -> 1: (halt' = true) ",i);
+            %fprintf(fid,"\t[finish] state=%d -> 1: (halt' = true) ",i);
+            fprintf(fid,"\t[finish] state=%d -> 1: (state' = 1) ",i);
         end
         
         fprintf(fid,";\n");
