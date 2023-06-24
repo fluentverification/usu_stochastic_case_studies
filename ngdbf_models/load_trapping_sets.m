899e1825d9b2e303d3e7f3dc3f73cf76d36ea7a7
Eric Reiss <reiss_eric@hotmail.com>
2023-06-24 13:40:45 -0600

    Starting trapping set script, make state range match error position

---
+++ ngdbf_models/load_trapping_sets.m
@@ -0,0 +1,56 @@
+
+% Trappping sets from University of Arizona
+% https://uweb.engr.arizona.edu/~vasiclab/Projects/CodingTheory/TrappingSetOntology.html
+% Diagrams on GitHub: 
+
+% Adjacency matrix structure
+%    s_1 s_2 s_3 ... s_n
+% c_1
+% c_2
+% c_3
+%  |
+% c_n
+
+three_three = [
+1 0 0 
+1 1 0
+0 1 0
+1 0 1
+0 1 1
+0 0 1
+];
+
+four_four = [    
+1 1 0 0
+0 1 1 0
+0 0 1 1
+1 0 0 1 
+1 0 0 0
+0 1 0 0
+0 0 1 0
+0 0 0 1
+];
+
+five_three = [
+1 1 0 0 0
+1 0 1 0 0
+1 0 0 1 0
+0 1 0 0 0
+0 0 1 0 0
+0 0 0 1 0
+0 1 0 0 1
+0 0 1 0 1 
+0 0 0 1 1
+];
+
+six_four_four = [
+    
+];
+
+six_four_eight = [
+    
+];
+
+six_two = [
+    
+];
