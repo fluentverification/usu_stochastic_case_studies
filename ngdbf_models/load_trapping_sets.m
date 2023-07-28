111a076b9d462ecd66bb8dbc84a907de3663dc07
Eric Reiss <reiss_eric@hotmail.com>
2023-07-28 13:40:43 -0600

    List trapping sets

--- ngdbf_models/load_trapping_sets.m
+++ ngdbf_models/load_trapping_sets.m
@@ -9,7 +9,8 @@
 %  |
 % c_n
 fprintf("\n\nTo use trapping sets call <strong>run_ngdbf(trapping_set,explicit_model,finish_condition)</strong> where trapping_set \nis one the loaded sets, explicit_model is a boolean\nthat is true if you would like to generate an explicit model \n(see PRISM manual for more info on explicit models\nhttps://www.prismmodelchecker.org/manual/Appendices/ExplicitModelFiles)\nor false if you would like to generate a normal .prism model. \nfinish_condition is a boolean that indicates you want the simulation to end if the all-zero state is reached.\n\n\n");
-
+fprintf("Currently Available Sets\n");
+fprintf("(3,3) = three_three\n");
 three_three = [
 1 0 0 
 1 1 0
@@ -19,6 +20,7 @@ three_three = [
 0 0 1
 ];
 
+fprintf("(4,4) = four_four\n");
 four_four = [    
 1 1 0 0
 0 1 1 0
@@ -30,6 +32,7 @@ four_four = [
 0 0 0 1
 ];
 
+fprintf("(5,3) = five_three\n");
 five_three = [
 1 1 0 0 0
 1 0 1 0 0
@@ -42,6 +45,7 @@ five_three = [
 0 0 0 1 1
 ];
 
+fprintf("(6,4){4} = six_four_four\n");
 six_four_four = [
 1 1 0 0 0 0
 1 0 0 1 0 0
@@ -56,6 +60,7 @@ six_four_four = [
 0 0 0 0 1 1
 ];
 
+fprintf("(6,4){8} = six_four_eight\n");
 six_four_eight = [
 1 0 0 1 0 0
 1 0 0 0 0 0
@@ -70,6 +75,7 @@ six_four_eight = [
 0 0 0 0 0 1
 ];
 
+fprintf("(6,2) = six_two\n");
 six_two = [
 1 1 0 0 0 0
 1 0 0 0 1 0
@@ -83,6 +89,7 @@ six_two = [
 0 0 0 1 0 1
 ];
 
+fprintf("(6,0) = six_zero\n");
 six_zero = [
 1 0 0 1 0 0
 1 0 0 0 1 0
@@ -95,6 +102,8 @@ six_zero = [
 0 0 1 0 0 1
 ];
 
+
+fprintf("(7,5){3} = seven_five_three\n");
 seven_five_three = [
 1 0 0 0 0 0 1
 1 0 0 0 1 0 0
@@ -110,6 +119,7 @@ seven_five_three = [
 0 0 0 0 0 1 1
 ];
 
+fprintf("(7,5){6} = seven_five_six\n")
 seven_five_six = [
 1 0 0 0 0 1 0
 1 0 0 0 0 0 1
@@ -126,6 +136,7 @@ seven_five_six = [
 0 0 0 0 0 0 1
 ];
 
+fprintf("(7,3){3} = seven_three_three\n");
 seven_three_three = [
 1 0 0 0 1 0 0
 1 0 0 0 0 1 0
@@ -140,6 +151,7 @@ seven_three_three = [
 0 0 0 0 0 0 1
 ];
 
+fprintf("(7,3){4} = seven_three_four\n");
 seven_three_four = [
 1 0 0 0 1 0 0 
 1 0 0 0 0 0 1
@@ -155,6 +167,7 @@ seven_three_four = [
 0 0 0 0 0 1 1
 ];
 
+fprintf("(7,3){10} = seven_three_ten");
 seven_three_ten = [
 1 0 0 1 0 0 0
 1 0 0 0 0 0 0
@@ -170,6 +183,7 @@ seven_three_ten = [
 0 1 1 0 0 0 0 
 ];
 
+fprintf("(7,1){1} = seven_one_one\n")
 seven_one_one = [
 1 0 0 0 1 0 0 
 1 0 0 0 0 1 0
@@ -184,6 +198,7 @@ seven_one_one = [
 0 1 1 0 0 0 0
 ];
 
+fprintf("(8,6){4} = eight_six_four\n");
 eight_six_four = [
 1 0 0 0 0 0 0 1
 1 0 0 0 0 1 0 0
