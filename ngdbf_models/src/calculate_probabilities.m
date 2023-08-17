c4982a0b9627dc7f65098edddc536fc418a68ae8
Eric Reiss <reiss_eric@hotmail.com>
2023-08-17 13:44:24 -0600

    reduce complexity

--- ngdbf_models/src/calculate_probabilities.m
+++ ngdbf_models/src/calculate_probabilities.m
@@ -1,5 +1,7 @@
 function [p,status] = calculate_probabilities(energies,theta,sigma,sym_size)
+    begin = time();
     p = ones(2^sym_size,2^sym_size);
+    p_test = ones(2^sym_size,2^sym_size);
     % Flip probabilities calculated according to Eq 3.13 in T. Tithi
     % dissertation (pg. 26)
     % Calculate individual bit flipping probabilities
@@ -9,21 +11,30 @@ function [p,status] = calculate_probabilities(energies,theta,sigma,sym_size)
         %rowbin = dec2bin(row-1,sym_size);
         for col = 1:2^sym_size
             %colbin = dec2bin(col-1,sym_size);
-            for p_idx = 1:sym_size
-                if bin_pos(row,p_idx) == bin_pos(col,p_idx)
-                    p(row,col) = p(row,col)*(1-p_flip(row,p_idx));
-                else
-                    p(row,col) = p(row,col)*p_flip(row,p_idx);
-                end
-            end
+            y = (bin_pos(row,:)==bin_pos(col,:));
+            p(row,col) = prod(abs(1-(mod(y+1,2)+p_flip(row,:))));
+            % for p_idx = 1:sym_size
+            %     if bin_pos(row,p_idx) == bin_pos(col,p_idx)
+            %         p(row,col) = p(row,col)*(1-p_flip(row,p_idx));
+            %     else
+            %         p(row,col) = p(row,col)*p_flip(row,p_idx);
+            %     end
+            % end
         end
     end
+    % p_test 
+    % p 
+    % p == p_test
+    % sum(p_test.')
+    % pause;
     % Sanity check
-    if sum(sum(p.')) ~= 2^sym_size
+    if sum(round(sum(p.'))) ~= 2^sym_size
         fprintf("Error: Probabilities do not sum to 1\n");
         sum(p.')
         status = -1;
     else
         status = 0;
     end
+    end_time = time();
+    fprintf("probability calculation took %d seconds\n",end_time-begin);
 endfunction
