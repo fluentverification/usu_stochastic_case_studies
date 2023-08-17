3a6668c8346c9db08a3673754e84de28fb92b05c
Eric Reiss <reiss_eric@hotmail.com>
2023-08-17 13:46:16 -0600

    start cmd line args

--- ngdbf_models/test.sh
+++ ngdbf_models/test.sh
@@ -1,4 +1,15 @@
 #! /bin/octave -qf
-addpath(genpath(pwd))
-load_trapping_sets
-run_ngdbf_prism(eight_eight)
+addpath(genpath(pwd));
+load_trapping_sets;
+
+# --engine [prism/storm] -e
+# --runs -n
+# --trapping-set -m
+# --sigma -d
+# --code-rate -R
+# --SNR -s 
+# --threshold -t
+# --property -p
+# --weight -w
+
+run_ngdbf_storm(eight_eight);
